98066da52eecc2c4cf6e51026d81f4a77bd8730f
Brody Sears <brodysears@Brodys-Mac-mini.local>
2022-01-23 13:23:12 -0700

    Added views, controllers and labels

---
+++ SportsTeams/SportsTeams/Controllers/Custom Views/SportsTeamsTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  SportsTeamsTableViewCell.swift
+//  SportsTeams
+//
+//  Created by Brody Sears on 1/23/22.
+//
+
+import UIKit
+
+class SportsTeamsTableViewCell: UITableViewCell {
+
+   
+}
