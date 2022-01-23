a5b40dcd9aa5d5e5441888286320a970ed509b07
Brody Sears <brodysears@Brodys-Mac-mini.local>
2022-01-23 14:28:03 -0700

    Added buttons, labels, outlets and actions along with its controller

---
+++ SportsTeams/SportsTeams/Controllers/SportsTeamsDetailViewController.swift
@@ -0,0 +1,37 @@
+//
+//  SportsTeamsDetailViewController.swift
+//  SportsTeams
+//
+//  Created by Brody Sears on 1/23/22.
+//
+
+import UIKit
+
+class SportsTeamsDetailViewController: UIViewController {
+
+// MARK: - Outlets
+    @IBOutlet weak var teamNameTextField: UITextField!
+    @IBOutlet weak var teamRankingTextField: UITextField!
+    @IBOutlet weak var teamPlayerCountTextField: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    // MARK: - Actions
+    @IBAction func saveButtonTapped(_ sender: Any) {
+    }
+    
+    @IBAction func clearButtonTapped(_ sender: Any) {
+    }
+    
+    @IBAction func deleteButtonTapped(_ sender: Any) {
+    }
+    
+
+   
+
+}// End of class
