9c1ed70b66f9fc4ff74153bd3369441679212dc8
Brody Sears <brodysears@Brodys-Mac-mini.local>
2022-01-23 12:53:34 -0700

    Initial commit

---
+++ SportsTeams/SportsTeams/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SportsTeams
+//
+//  Created by Brody Sears on 1/23/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
